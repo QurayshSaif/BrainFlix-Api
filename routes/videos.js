37fa9e7a3fc75d4dd2fb38fb5e5580b0f7efd0f1
QurayshSaif <saifquraysh@gmaill.com>
2023-03-19 14:42:50 -0400

    post comments and delete comments endpoints added

--- routes/videos.js
+++ routes/videos.js
@@ -69,4 +69,51 @@ router.get("/:id", (req, res) => {
   }
 });
 
+// POST a new comment to a video
+router.post("/:id/comments", (req, res) => {
+  const videoId = req.params.id;
+  const video = videoDetails.find((v) => v.id === videoId);
+
+  if (!video) {
+    res.status(404).send("Video not found");
+  } else {
+    const commentObj = req.body;
+    const newComment = {
+      id: uuid(),
+      name: commentObj.name,
+      comment: commentObj.comment,
+      likes: 0,
+      timestamp: new Date(),
+    };
+    video.comments.push(newComment);
+
+    fs.writeFileSync(FILE_PATH, JSON.stringify(videoDetails));
+
+    res.status(201).json(newComment);
+  }
+});
+
+// DELETE a comment from a video
+router.delete("/:videoId/comments/:commentId", (req, res) => {
+  const videoId = req.params.videoId;
+  const commentId = req.params.commentId;
+  const video = videoDetails.find((v) => v.id === videoId);
+
+  if (!video) {
+    res.status(404).send("Video not found");
+  } else {
+    const commentIndex = video.comments.findIndex((c) => c.id === commentId);
+
+    if (commentIndex === -1) {
+      res.status(404).send("Comment not found");
+    } else {
+      video.comments.splice(commentIndex, 1);
+
+      fs.writeFileSync(FILE_PATH, JSON.stringify(videoDetails));
+
+      res.sendStatus(204);
+    }
+  }
+});
+
 module.exports = router;
